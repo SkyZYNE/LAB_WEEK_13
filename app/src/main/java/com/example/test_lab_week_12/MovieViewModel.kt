65e4509ee3d102de39ed5906898071374dd8cf41
SkyZYNE <raffaelgunawan19@gmail.com>
2025-11-24 09:32:38 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import java.util.Calendar
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
     // define the StateFlow in replace of the LiveData
@@ -30,13 +31,21 @@ class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel()
     // launch a coroutine in viewModelScope
     // Dispatchers.IO means that this coroutine will run on a shared pool of threads
         viewModelScope.launch(Dispatchers.IO) {
-            movieRepository.fetchMovies().catch {
+            movieRepository.fetchMovies().catch { exception ->
                 // catch is a terminal operator that catches exceptions from the Flow
-                _error.value = "An exception occurred: ${it.message}"
-            }.collect {
+                _error.value = "An exception occurred: ${exception.message}"
+            }.collect { movies ->
+                val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
                 // collect is a terminal operator that collects the values from the Flow
                 // the results are emitted to the StateFlow
-                _popularMovies.value = it
+                _popularMovies.value =
+                    movies
+                        .filter { movie ->
+                            movie.releaseDate?.startsWith(currentYear) == true
+                        }
+                        .sortedByDescending { movie ->
+                            movie.popularity
+                        }
             }
         }
     }
