c9d6e6b03b6a544c1361490cdcab33b72e84fea0
SkyZYNE <raffaelgunawan19@gmail.com>
2025-12-01 16:31:11 +0700

    Commit No. 2 – update to Room

--- app/src/main/java/com/example/lab_week_13/model/Movie.kt
+++ app/src/main/java/com/example/lab_week_13/model/Movie.kt
@@ -1,10 +1,12 @@
 package com.example.lab_week_13.model
 
+import androidx.room.Entity
 import com.squareup.moshi.Json
 
+@Entity(tableName = "movies", primaryKeys = [("id")])
 data class Movie(
     val adult: Boolean = false,
-    val backdrop_path: String? = null,
+    val backdrop_path: String? = "",
     val id: Int = 0,
     @field:Json(name = "original_language")
     val originalLanguage: String? = null,
